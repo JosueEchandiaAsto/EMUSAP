491b350aeb6ceedb566d904c36476f354179903a
Josue Manuel Echandia Asto <josueechandiasto@gmail.com>
2016-04-14 17:58:15 -0500

    Agregado los Slides.css y Slides.js Agregar imagenes y diseño en photosoft de Slides Agregados responsiveslides.js Y agregamos RSLIDES en HTML

---
+++ js/index.js
@@ -0,0 +1,18 @@
+(function() {
+  $(window).scroll(function(){
+  var nav = $('#nav'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) nav.addClass('fixed-nav');
+  else nav.removeClass('fixed-nav');
+  });
+
+  $(".rslides").responsiveSlides({
+    auto: true,
+    pager: true,
+    nav: true,
+    speed: 500,
+    maxwidth: 1000,
+    namespace: "sli"
+  });
+})()
